6b3436aa2dc4ec2ecdaa2fcafb0ac4036d52dc61
Mitzi Song <mitzisong@gmail.com>
2016-11-22 22:03:05 -0800

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def hello
+  	render text: "¡hola, mundo!"
+  end
+  def goodbye
+  	render text: "¡ciao, mundo!"
+  end
+end
+
